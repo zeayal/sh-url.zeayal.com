11b316386e4167cdf00ac628a2baf62f3c60e605
ziyang.liu <ziyang.liu@mobilemd.cn>
2021-03-13 19:24:06 +0800

    add: validate

--- index.js
+++ index.js
@@ -1,44 +1,68 @@
-const express = require('express');
-const morgan = require('morgan');
-const {nanoid} = require('nanoid');
-
-const monk = require('monk');
+const express = require("express");
+const morgan = require("morgan");
+const { nanoid } = require("nanoid");
+const yup = require("yup");
+const monk = require("monk");
 const app = express();
 
-require('dotenv').config();
+require("dotenv").config();
 
 const db = monk(process.env.MONGODB_URL);
-const urls = db.get('urls');
-urls.createIndex({slug: 1}, {unique: true});
+const urls = db.get("urls");
+urls.createIndex({ slug: 1 }, { unique: true });
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms")
+);
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// app.get('/', (req, res) => {
-//     res.json("hello json");
-// })
+app.get("/", (req, res) => {
+  res.json("hello json");
+});
 
+// const urlSchema = yup.object().shape({
+//   url: yup.string().trim().url().required(),
+//   slug: yup.string().trim(),
+// });
 
-app.post('/url', (req, res) => {
-    let {url, slug} = req.body;
-    console.log('url body', req.body);
-    if(!slug) {
+const schema = yup.object().shape({
+    slug: yup.string().trim().matches(/^[\w\-]+$/i),
+    url: yup.string().trim().url().required(),
+  });
+  
+
+app.post("/url", async (req, res) => {
+  let { url, slug } = req.body;
+  console.log("url body", req.body);
+
+  try {
+    const valid = await schema.validate({ url, slug });
+    if (valid) {
+      if (!slug) {
         slug = nanoid(5);
-    }
-    urls.insert({
+      }
+      urls.insert({
         url,
         slug,
-    })
+      });
 
-    res.json({
+      res.json({
         status: 0,
-        message: '新增成功'
-    })
-})
+        message: "新增成功",
+      });
+    }
+  } catch (error) {
+    console.log("error", error);
+    res.json({
+      status: 1,
+      message: "新增失败2",
+    });
+  }
+});
 
 const PORT = process.env.PORT || 1323;
 
 app.listen(PORT, () => {
-    console.log(`server is runing in: http://localhost:${PORT}`)
-})
+  console.log(`server is runing in: http://localhost:${PORT}`);
+});
