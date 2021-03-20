86340502174840a3d4d2d274b7cb5fcf57b3c96d
ziyang.liu <ziyang.liu@mobilemd.cn>
2021-03-20 14:12:56 +0800

    add: complete function

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const { nanoid } = require("nanoid");
 const yup = require("yup");
 const monk = require("monk");
 const app = express();
-
+const fs = require("fs");
+const path = require("path");
+const fetch = require("node-fetch");
 require("dotenv").config();
 
 const db = monk(process.env.MONGODB_URL);
 const urls = db.get("urls");
 urls.createIndex({ slug: 1 }, { unique: true });
 
+app.set("view engine", "pug");
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms")
 );
@@ -21,44 +24,123 @@ app.get("/", (req, res) => {
   res.json("hello json");
 });
 
-// const urlSchema = yup.object().shape({
-//   url: yup.string().trim().url().required(),
-//   slug: yup.string().trim(),
-// });
+app.get("/error", (req, res) => {
+  throw new Error("test error message");
+});
+
+app.get("/file", (req, res, next) => {
+  fs.readFile(path.resolve(__dirname, "hello。.txt"), function (err, data) {
+    if (err) {
+      console.log("没有抛出异常，可能会中断程序 err");
+      next(err);
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+app.get("/github", async (req, res, next) => {
+  const response = await fetch("https://api.github.com/users/zeayal");
+  console.log("response", response.ok);
+  const data = await response.json();
+  res.json({
+    status: 0,
+    data,
+  });
+});
 
 const schema = yup.object().shape({
-    slug: yup.string().trim().matches(/^[\w\-]+$/i),
-    url: yup.string().trim().url().required(),
-  });
-  
+  url: yup.string().trim().url().required(),
+  slug: yup
+    .string()
+    .trim()
+    .matches(/^[\w\-]+$/i),
+});
 
-app.post("/url", async (req, res) => {
-  let { url, slug } = req.body;
-  console.log("url body", req.body);
+const slugSchema = yup.object().shape({
+  slug: yup.string().trim().matches(/^[\w\-]+$/i),
+});
 
+app.get(
+  "/writeToFile",
+  function (req, res, next) {
+    fs.writeFile("hello3.txt", "test data", next);
+  },
+  function (req, res) {
+    res.send("OK");
+  }
+);
+
+app.get("/mustCatchError", function (req, res, next) {
+  setTimeout(() => {
+    //
+    Promise.resolve(1)
+      .then((data) => {
+        console.log("data", data);
+        // res.send("ok");
+        throw new Error("test error");
+      })
+      .catch(next);
+  }, 100);
+});
+
+const notFoundPath = path.resolve('public/404.html')
+
+app.get("/:id", async (req, res, next) => {
+  let { id: slug } = req.params;
+  console.log('req.query', req.query)
+  console.log('req.params', req.params);
   try {
+    const valid = await slugSchema.validate({ slug });
+    if (valid) {
+      const data = await urls.findOne({ slug });
+      res.redirect(data.url);
+    } else {
+      res.status(404).sendFile(notFoundPath);
+    }
+  } catch (e) {
+    res.status(404).sendFile(notFoundPath);
+  }
+});
+
+app.post("/url", async (req, res, next) => {
+  let { url, slug } = req.body;
+  try {
+    if (!slug) {
+      slug = nanoid(5);
+    }
     const valid = await schema.validate({ url, slug });
     if (valid) {
-      if (!slug) {
-        slug = nanoid(5);
+      const existing = await urls.findOne({ slug });
+      if (existing) {
+        throw new Error("Slug in use. 🍔");
       }
-      urls.insert({
+      const data = await urls.insert({
         url,
         slug,
       });
 
       res.json({
         status: 0,
+        data,
         message: "新增成功",
       });
     }
   } catch (error) {
-    console.log("error", error);
-    res.json({
-      status: 1,
-      message: "新增失败2",
-    });
+    next(error);
   }
+});
+
+app.use((error, req, res, next) => {
+  if (error.status) {
+    res.status(500);
+  } else {
+    res.status(500);
+  }
+  res.json({
+    status: 1,
+    message: process.env.NODE_ENV === "production" ? "🍔" : error.stack,
+  });
 });
 
 const PORT = process.env.PORT || 1323;
--- public/app.js
+++ public/app.js
@@ -1,11 +1,31 @@
 const app = Vue.createApp({
-    data() {
-        return { count: 1 }
-    },
-    created() {
-        // this points to the vm instance
-        console.log(`count is: ` + this.count); // count is 1 
-    }
-})
+  data() {
+    return { url: "", slug: "", created: '' };
+  },
+  created() {
+    // this points to the vm instance
+  },
+  methods: {
+    async onSubmit() {
+      console.log("onSubmit", this.url, this.slug);
+      const { url, slug } = this;
+      const params = { url, slug };
+      console.log("params", params);
+      const res = await fetch("http://localhost:1323/url", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(params),
+      }).then((res) => res.json());
+  
+      console.log("res", res);
 
-const vm = app.mount('#root')
+      if(res.status === 0) {
+        this.created = `${location.origin}/${res.data.slug}`
+      }
+    },
+  },
+});
+
+const vm = app.mount("#root");
