008300124726681970a02c4918349b7178f4518d
ziyang.liu <zeayalsir@gmail.com>
2021-06-09 12:48:23 +0800

    feature：add Dockerfile

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const path = require("path");
 const fetch = require("node-fetch");
 require("dotenv").config();
 
-const db = monk(process.env.MONGODB_URL);
-const urls = db.get("urls");
-urls.createIndex({ slug: 1 }, { unique: true });
+// const db = monk(process.env.MONGODB_URL);
+// const urls = db.get("urls");
+// urls.createIndex({ slug: 1 }, { unique: true });
 
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms")
@@ -19,86 +19,90 @@ app.use(
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/github", async (req, res, next) => {
-  const response = await fetch("https://api.github.com/users/zeayal");
-  console.log("response", response.ok);
-  const data = await response.json();
-  res.json({
-    status: 0,
-    data,
-  });
-});
+// app.get("/github", async (req, res, next) => {
+//   const response = await fetch("https://api.github.com/users/zeayal");
+//   console.log("response", response.ok);
+//   const data = await response.json();
+//   res.json({
+//     status: 0,
+//     data,
+//   });
+// });
 
-app.use(
-  "/scripts",
-  express.static(__dirname + "/node_modules/@pnotify/")
-);
-app.use(
-  "/style",
-  express.static(__dirname + "/node_modules/@pnotify/")
-);
+// app.use(
+//   "/scripts",
+//   express.static(__dirname + "/node_modules/@pnotify/")
+// );
+// app.use(
+//   "/style",
+//   express.static(__dirname + "/node_modules/@pnotify/")
+// );
 
-const schema = yup.object().shape({
-  url: yup.string().trim().url().required(),
-  slug: yup
-    .string()
-    .trim()
-    .matches(/^[\w\-]+$/i),
-});
+// const schema = yup.object().shape({
+//   url: yup.string().trim().url().required(),
+//   slug: yup
+//     .string()
+//     .trim()
+//     .matches(/^[\w\-]+$/i),
+// });
 
-const slugSchema = yup.object().shape({
-  slug: yup
-    .string()
-    .trim()
-    .matches(/^[\w\-]+$/i),
-});
+// const slugSchema = yup.object().shape({
+//   slug: yup
+//     .string()
+//     .trim()
+//     .matches(/^[\w\-]+$/i),
+// });
 
-const notFoundPath = path.resolve("public/404.html");
+// const notFoundPath = path.resolve("public/404.html");
 
-app.get("/:id", async (req, res, next) => {
-  let { id: slug } = req.params;
-  console.log("req.query", req.query);
-  console.log("req.params", req.params);
-  try {
-    const valid = await slugSchema.validate({ slug });
-    if (valid) {
-      const data = await urls.findOne({ slug });
-      res.redirect(data.url);
-    } else {
-      res.status(404).sendFile(notFoundPath);
-    }
-  } catch (e) {
-    res.status(404).sendFile(notFoundPath);
-  }
-});
+// app.get("/:id", async (req, res, next) => {
+//   let { id: slug } = req.params;
+//   console.log("req.query", req.query);
+//   console.log("req.params", req.params);
+//   try {
+//     const valid = await slugSchema.validate({ slug });
+//     if (valid) {
+//       const data = await urls.findOne({ slug });
+//       res.redirect(data.url);
+//     } else {
+//       res.status(404).sendFile(notFoundPath);
+//     }
+//   } catch (e) {
+//     res.status(404).sendFile(notFoundPath);
+//   }
+// });
 
-app.post("/url", async (req, res, next) => {
-  let { url, slug } = req.body;
-  try {
-    if (!slug) {
-      slug = nanoid(5);
-    }
-    const valid = await schema.validate({ url, slug });
-    if (valid) {
-      const existing = await urls.findOne({ slug });
-      if (existing) {
-        throw new Error("Slug in use. 🍔");
-      }
-      const data = await urls.insert({
-        url,
-        slug,
-      });
+// app.post("/url", async (req, res, next) => {
+//   let { url, slug } = req.body;
+//   try {
+//     if (!slug) {
+//       slug = nanoid(5);
+//     }
+//     const valid = await schema.validate({ url, slug });
+//     if (valid) {
+//       const existing = await urls.findOne({ slug });
+//       if (existing) {
+//         throw new Error("Slug in use. 🍔");
+//       }
+//       const data = await urls.insert({
+//         url,
+//         slug,
+//       });
 
-      res.json({
-        status: 0,
-        data,
-        message: "新增成功",
-      });
-    }
-  } catch (error) {
-    next(error);
-  }
-});
+//       res.json({
+//         status: 0,
+//         data,
+//         message: "新增成功",
+//       });
+//     }
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+app.get('/hello', (req,res) => {
+  res.send(`hello, I'm in docker`);
+})
 
 app.use((error, req, res, next) => {
   if (error.status) {
